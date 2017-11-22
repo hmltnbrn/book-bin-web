bcf43690d9afbca74788ab8b6ef2b792f60992a1
Brian Hamilton <hmltnbrn@gmail.com>
2017-11-21 23:16:48 -0500

    Reusable top logo scss

--- src/app/components/internal-error/internal-error.component.ts
+++ src/app/components/internal-error/internal-error.component.ts
@@ -1,10 +1,9 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-internal-error',
   templateUrl: './internal-error.component.html',
-  styleUrls: ['./internal-error.component.scss'],
-  encapsulation: ViewEncapsulation.Emulated
+  styleUrls: ['./internal-error.component.scss']
 })
 export class InternalErrorComponent implements OnInit {
 
